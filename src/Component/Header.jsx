491ef8924c015351eb6995cd70a0f555c844527b
wacho91 <criskol.71@gmail.com>
2024-12-09 11:59:34 -0500

    Logo & Navegation Header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,57 @@
 
+import { HiHome, HiPlus, HiStar } from "react-icons/hi";
+import Logo from "../assets/images/logo.png";
+import { HiMagnifyingGlass, HiPlayCircle, HiTv } from "react-icons/hi2";
+import HeaderItem from "./HeaderItem";
+import { useState } from "react";
+
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+    const menu = [
+        {
+            name: "Home",
+            icon: HiHome
+        },
+
+        {
+            name: "Search",
+            icon: HiMagnifyingGlass
+        },
+
+        {
+            name: "Watch List",
+            icon: HiPlus
+        },
+
+        {
+            name: "Originals",
+            icon: HiStar
+        }, 
+
+        {
+            name: "Movies",
+            icon: HiPlayCircle
+        },
+
+        {
+            name: "Series",
+            icon: HiTv
+        }
+    ]
+
   return (
     <div>
-        Header Component
+        <div>
+            <img src={Logo} alt="" />
+            <div>
+                {/*Maping in the menu*/}
+                {menu.map((item)=>(
+                    <HeaderItem name={item.name} Icon={item.icon} />
+                ))}
+            </div>
+        </div>
+        
     </div>
   )
 }
