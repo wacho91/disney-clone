d1af4bf313a4c5ed106ee0f7e1c6d486a6a7ee54
wacho91 <criskol.71@gmail.com>
2024-12-09 11:31:32 -0500

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./Component/Header"
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header />
+    </div>
   )
 }
 
