0da5a6e7e4709088048dbf580f94db7971220a5f
wacho91 <criskol.71@gmail.com>
2024-12-10 11:57:51 -0500

    Production House Component Complete

--- src/Component/ProductionHouse.jsx
+++ src/Component/ProductionHouse.jsx
@@ -46,12 +46,22 @@ const ProductionHouse = () => {
     ]
 
   return (
-    <div>
+    <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16">
         {productionHouseList.map((item) => (
-          <div key={item.id}>
-            <video src={item.video} />
-            <img src={item.image} alt="" />
-          </div>  
+            <div
+                className="border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300
+                ease-in-out cursor-pointer relative shadow-xl shadow-gray-800" 
+                key={item.id}
+            >
+                <video 
+                    src={item.video} autoPlay loop playsInline muted  
+                    className="absolute z-0  top-0 rounded-md opacity-0 hover:opacity-50"
+                />
+                <img 
+                    src={item.image} 
+                    className="w-full z-[1] opacity-100"
+                />
+            </div>  
         ))}
     </div>
   )
