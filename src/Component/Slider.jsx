12fd46349c481481dc1df9db12c8240d9b913f29
wacho91 <criskol.71@gmail.com>
2024-12-10 11:16:40 -0500

    SliderArrow complete

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 import GlobalApi from "../Services/GlobalApi"
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
 const screenWidth=window.innerWidth;
 
@@ -7,6 +8,10 @@ const Slider = () => {
 
     const [ movieList, setMovieList ] = useState([]);
     const elementRef=useRef();
+
+    useEffect(() => {
+        getTrendingMovies();
+    },[])
 
     const getTrendingMovies = () => {
         GlobalApi.getTrendingVideos.then( res =>  {
@@ -23,8 +28,19 @@ const Slider = () => {
     }
 
   return (
-    <div>
-        Slider Component 
+    <div>   
+        <HiChevronLeft 
+            className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+            onClick={()=>sliderLeft(elementRef.current)}
+        />
+
+        <HiChevronRight 
+            className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+            onClick={()=>sliderRight(elementRef.current)}
+        />
+        <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth">
+            
+        </div>
     </div>
   )
 }
