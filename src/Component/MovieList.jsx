d10c2be580784752bad00d22015e0279e1622841
wacho91 <criskol.71@gmail.com>
2024-12-11 10:50:47 -0500

    MovieCard Complete

--- src/Component/MovieList.jsx
+++ src/Component/MovieList.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useRef, useState } from "react"
+import GlobalApi from "../Services/GlobalApi"
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5"
+import MovieCard from "./MovieCard"
+MovieCard
 
+IoChevronBackCircleOutline
 const MovieList = () => {
   return (
     <div>
